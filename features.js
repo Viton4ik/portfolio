b6c3da471f1c6a1181d93f415262524e7480906b
Vict0r <viton4ik@gmail.com>
2023-04-09 18:19:19 +0200

    rev.01

--- features.js
+++ features.js
@@ -1,13 +1,15 @@
 
-
 const introSection = document.querySelector('#intro')
 const aboutSection = document.querySelector('#about')
 const projectsSection = document.querySelector('#projects')
+const contactSection = document.querySelector('#contact')
 
 // scroll handle
 window.addEventListener('scroll', function() {
     const aboutSectionTop = aboutSection.getBoundingClientRect().top
-    // const projectsSectionTop = projectsSection.getBoundingClientRect().top
+    const introSectionTop = introSection.getBoundingClientRect().top
+    const projectsSectionTop = projectsSection.getBoundingClientRect().top
+    const contactSectionTop = contactSection.getBoundingClientRect().top
     const windowHeight = window.innerHeight
 
     // delay for scrolling
@@ -16,42 +18,35 @@ window.addEventListener('scroll', function() {
             // aboutSection.style.opacity = '1'
             // aboutSection.style.transition = 'opacity 1s ease'
             aboutSection.style.animation = 'fadeIn 1s ease-in-out 0.6s forwards'
-            setTimeout(() => {
-                // projectsSection.style.opacity = '1'
-                // projectsSection.style.transition = 'opacity 1s ease'
-                projectsSection.style.animation = 'fadeIn 1s ease-in-out 0.5s forwards'
-            }, 400)
-        }, 200)
+            // setTimeout(() => {
+            //     // projectsSection.style.opacity = '1'
+            //     // projectsSection.style.transition = 'opacity 1s ease'
+            //     projectsSection.style.animation = 'fadeIn 1s ease-in-out 0.5s forwards'
+            // }, 150)
+        }, 100)
 
     }
 
-    // let scrollTop = window.scrollY
-    // let introSectionOffsetTop = introSection.getBoundingClientRect().top
-    // let aboutSectionOffsetTop = aboutSection.getBoundingClientRect().top
-    // // let introSectionOffsetTop = introSection.offsetTop
-    // // let aboutSectionOffsetTop = aboutSection.offsetTop
-    // // var section3Top = section3.offsetTop
-    // let introSectionOffsetHeight = introSection.offsetHeight
-    // let aboutSectionOffsetHeight = aboutSection.offsetHeight
-    // // var section3Height = section3.offsetHeight
-    //
-    // // if (scrollTop >= introSectionOffsetTop && scrollTop < introSectionOffsetTop + introSectionOffsetHeight) {
-    //     if (introSectionOffsetTop < windowHeight) {
-    //     aboutSection.style.opacity = '1'
-    //     aboutSection.style.transition = 'opacity 1s ease'
-    //     introSection.style.opacity = '0'
-    //     // introSection.style.display = 'block'
-    //     // aboutSection.style.display = 'none'
-    //     // section3.style.display = 'none'
-    // // } else if (scrollTop >= aboutSectionOffsetTop && scrollTop < aboutSectionOffsetTop + aboutSectionOffsetHeight) {
-    //     } else if (aboutSectionOffsetTop < windowHeight) {
-    //     introSection.style.opacity = '1'
-    //     introSection.style.transition = 'opacity 1s ease'
-    //     aboutSection.style.opacity = '0'
-    //     // introSection.style.display = 'none'
-    //     // aboutSection.style.display = 'block'
-    //     // section3.style.display = 'none'
-    // }
+    // if (introSectionTop < windowHeight) {
+    //     setTimeout(() => {
+    //         introSection.style.animation = 'fadeIn 1s ease-in-out 0.6s forwards'
+    //     }, 50)
+
+
+    if (projectsSectionTop < windowHeight) {
+        setTimeout(() => {
+            projectsSection.style.animation = 'fadeIn 1s ease-in-out 0.6s forwards'
+    }, 100)
+
+    }
+
+    if (contactSectionTop < windowHeight) {
+        setTimeout(() => {
+            contactSection.style.animation = 'fadeIn 1s ease-in-out 0.6s forwards'
+            // contactSection_.style.display = 'block'
+        }, 100)
+    }
+
 })
 
 
@@ -70,54 +65,44 @@ for (let i = 0; i < coll.length; i++) {
     })
 }
 
-
-// // overlay
-// function on() {
-//     document.getElementById("overlay").style.display = "block"
-//     // document.querySelector(".overlay").style.display = "block"
-// }
-//
-// function off() {
-//     document.getElementById("overlay").style.display = "none"
-//     // document.querySelector(".overlay").style.display = "none"
-// }
-
-// var mySlidesNumber = 'mySlides1'
-// var dotNumber = 'dot1'
-// overlay
-function on(blockName, mySlidesNum) {
+//slides' buttons
+function on(blockName, mySlidesNum, myDotsNum) {
     // get set of the slides for the specific block
     let mySlidesNumber = mySlidesNum
+    let myDotsNumber = myDotsNum
     let slideIndex = 1
-    showSlides(slideIndex, mySlidesNumber)
+    showSlides(slideIndex, mySlidesNumber, myDotsNumber)
     //show the block
     document.getElementById(blockName).style.display = "block"
 }
 
 function off(blockName) {
     document.getElementById(blockName).style.display = "none"
+    // to avoid slide bugging
+    currentSlide(1, 'mySlides1', 'dot1')
 }
 
 
 //slider bar
 let mySlidesNumber = 'mySlides1'
+let myDotsNumber = 'dot1'
 let slideIndex = 1
-showSlides(slideIndex, mySlidesNumber)
+showSlides(slideIndex, mySlidesNumber, myDotsNumber)
 
 // Next/previous controls
-function plusSlides(n, mySlidesNumber) {
-    showSlides(slideIndex += n, mySlidesNumber)
+function plusSlides(n, mySlidesNumber, myDotsNumber) {
+    showSlides(slideIndex += n, mySlidesNumber, myDotsNumber)
 }
 
 // Thumbnail image controls
-function currentSlide(n, mySlidesNumber, dotNumber) {
-    showSlides(slideIndex = n, mySlidesNumber, dotNumber)
+function currentSlide(n, mySlidesNumber, myDotsNumber) {
+    showSlides(slideIndex = n, mySlidesNumber, myDotsNumber)
 }
 
-function showSlides(n, mySlidesNumber) {
+function showSlides(n, mySlidesNumber, myDotsNumber) {
     let i;
     let slides = document.getElementsByClassName(`${mySlidesNumber}`)
-    let dots = document.getElementsByClassName(`dot`)
+    let dots = document.getElementsByClassName(`${myDotsNumber}`)
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
